799efdb065ff7433307962a209299a7f1bd53cb5
max-wn <masaraksh@icloud.com>
2021-10-11 18:42:02 +0300

    qtile config
    minor changes for qtile config

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -5,7 +5,7 @@
 # Copyright (c) 2012 Craig Barnes
 # Copyright (c) 2013 horsik
 # Copyright (c) 2013 Tao Sauvage
-#
+# Latest commit 5996e09 on Aug 31, 2020
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -33,54 +33,53 @@ from libqtile.utils import guess_terminal
 
 mod = "mod4"
 terminal = guess_terminal()
-#myConfig = "/home/nils/.config/qtile/config.py"  # Qtile config file location
 
 keys = [
-# Switch between windows
-Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-Key([mod], "space", lazy.layout.next(),
-    desc="Move window focus to other window"),
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
 
-# Move windows between left/right columns or move up/down in current stack.
-# Moving out of range in Columns layout will create new column.
-Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-    desc="Move window to the left"),
-Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-    desc="Move window to the right"),
-Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-    desc="Move window down"),
-Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
-# Grow windows. If current window is on the edge of screen and direction
-# will be to screen edge - window would shrink.
-Key([mod, "control"], "h", lazy.layout.grow_left(),
-    desc="Grow window to the left"),
-Key([mod, "control"], "l", lazy.layout.grow_right(),
-    desc="Grow window to the right"),
-Key([mod, "control"], "j", lazy.layout.grow_down(),
-    desc="Grow window down"),
-Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
-# Toggle between split and unsplit sides of stack.
-# Split = all windows displayed
-# Unsplit = 1 window displayed, like Max layout, but still with
-# multiple stack panes
-Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-    desc="Toggle between split and unsplit sides of stack"),
-Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
 
-# Toggle between different layouts as defined below
-Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
 
-Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-Key([mod], "r", lazy.spawncmd(),
-    desc="Spawn a command using a prompt widget"),
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -89,11 +88,11 @@ for i in groups:
     keys.extend([
         # mod1 + letter of group = switch to group
         Key([mod], i.name, lazy.group[i.name].toscreen(),
-        desc="Switch to group {}".format(i.name)),
+            desc="Switch to group {}".format(i.name)),
 
         # mod1 + shift + letter of group = switch to & move focused window to group
         Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-        desc="Switch to & move focused window to group {}".format(i.name)),
+            desc="Switch to & move focused window to group {}".format(i.name)),
         # Or, use below if you prefer not to switch to that group.
         # # mod1 + shift + letter of group = move focused window to group
         # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
@@ -101,19 +100,20 @@ for i in groups:
     ])
 
 layouts = [
-layout.Columns(border_focus_stack='#d75f5f'),
-layout.Max(),
-# Try more layouts by unleashing below layouts.
-# layout.Stack(num_stacks=2),
-# layout.Bsp(),
-# layout.Matrix(),
-# layout.MonadTall(),
-# layout.MonadWide(),
-# layout.RatioTile(),
-# layout.Tile(),
-# layout.TreeTab(),
-# layout.VerticalTile(),
-# layout.Zoomy(),
+    layout.Columns(border_focus_stack=['#d75f5f', '#8f3d3d'], border_width=4),
+    layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+    layout.Floating()
 ]
 
 widget_defaults = dict(
@@ -124,56 +124,54 @@ widget_defaults = dict(
 extension_defaults = widget_defaults.copy()
 
 screens = [
-Screen(
-    top=bar.Bar(
-        [
-            widget.CurrentLayout(),
-            widget.GroupBox(),
-            widget.Prompt(),
-            widget.WindowName(),
-            widget.Chord(
-                chords_colors={
-                    'launch': ("#ff0000", "#ffffff"),
-                },
-                name_transform=lambda name: name.upper(),
-            ),
-            widget.TextBox("CNFG: my", name="default"),
-            widget.TextBox(text = "KBLO:"),
-            widget.KeyboardLayout(),
-            widget.TextBox(text = "BTTR:"),
-            widget.Battery(format = '{char} {percent:2.0%}', unknown_char = '#'),
-            widget.TextBox(text = "PCMN"),
-            widget.CheckUpdates(
-                update_interval = 1800,
-                distro = "Arch_checkupdates",
-                display_format = 'UPD: {updates}',
-                no_update_string = '#'),
-            #widget.TextBox(text = "updates"),
-            widget.TextBox(text = "VOL:"),
-            widget.Volume(),
-            #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-            widget.Systray(),
-            widget.TextBox( text = "DATE:"),
-            widget.Clock(format='%Y.%m.%d %a %H:%M'),
-            #widget.QuickExit(),
-        ],
-        24,
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                #widget.TextBox("default config", name="default"),
+                #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                #widget.KeyboardLayout(),
+                widget.TextBox("BTTR:"),
+                widget.Battery(format = '{char} {percent:2.0%}', unknown_char = '#'),
+                widget.TextBox(text = "PCMN"),
+                widget.CheckUpdates(
+                    update_interval = 1800,
+                    distro = "Arch_checkupdates",
+                    display_format = 'UPD: {updates}',
+                    no_update_string = '#'),
+                #widget.TextBox(text = "updates"),
+                widget.TextBox(text = "VOL:"),
+                widget.Volume(),
+                #widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+                widget.QuickExit(),
+            ],
+            24,
+        ),
     ),
-),
 ]
 
 # Drag floating layouts.
 mouse = [
-Drag([mod], "Button1", lazy.window.set_position_floating(),
-     start=lazy.window.get_position()),
-Drag([mod], "Button3", lazy.window.set_size_floating(),
-     start=lazy.window.get_size()),
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
-main = None  # WARNING: this is deprecated and will be removed soon
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
@@ -189,6 +187,11 @@ floating_layout = layout.Floating(float_rules=[
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
